cb66697fa8c12d083b9018749bd3126ce3b92b56
Sisig <87691869+SisigSarino@users.noreply.github.com>
2023-11-08 17:36:49 +0800

    Update chat.ts

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -20,7 +20,7 @@ Respond using Markdown.`;
 export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo',
   'gpt-3.5-turbo-16k',
-  'gpt-3.5-turbo-1106',
+  'gpt-3.5-turbo-1106', //update
   'gpt-4',
   'gpt-4-32k',
   'gpt-4-1106-preview'
@@ -68,7 +68,7 @@ export const modelCost = {
     prompt: { price: 0.003, unit: 1000 },
     completion: { price: 0.004, unit: 1000 },
   },
-  'gpt-3.5-turbo-1106':{
+  'gpt-3.5-turbo-1106':{ //update
     prompt: { price: 0.001, unit: 1000 },
     completion: { price: 0.002, unit: 1000 },
   },
