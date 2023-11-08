b6326cfd254af3b07945465ae02062a5dc9d66db
Sisig <87691869+SisigSarino@users.noreply.github.com>
2023-11-08 17:49:08 +0800

    Update chat.ts

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -20,7 +20,7 @@ Respond using Markdown.`;
 export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo',
   'gpt-3.5-turbo-16k',
-  'gpt-3.5-turbo-1106-preview', //update
+  'gpt-3.5-turbo-1106-preview', //update eah!!
   'gpt-4',
   'gpt-4-32k',
   'gpt-4-1106-preview'
