144303f1719be6252fa125879293d64314816534
resurgent2024 <150219998+resurgent2024@users.noreply.github.com>
2023-11-11 11:35:03 +0800

    Update chat.ts
    ye

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -22,6 +22,8 @@ export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo-16k',
   'gpt-3.5-turbo-1106', //update eah!!
   'gpt-4',
+  'gpt-4-0314',
+  'gpt-4-32k-0314',
   'gpt-4-32k',
   'gpt-4-1106-preview'
   // 'gpt-3.5-turbo-0301',
